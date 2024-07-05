bdea201cff6d8a95b1622bfc2a37bd22138f4b9a
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-07-05 13:18:13 -0600

    new controller options

--- src/controllers/versionController.js
+++ src/controllers/versionController.js
@@ -1,11 +1,20 @@
-const versions = require('../models/versionModel');
+const { getVersionsFromGitHub } = require('../models/versionModel');
 
-const getAllVersions = (req, res) => {
+const getNaisTrainingDiaryVersions = async (req, res) => {
+    const versions = await getVersionsFromGitHub('SpanishHgames', 'NaisTrainingDiary');
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(versions));
+    res.end(JSON.stringify(versions, null, 2));
+};
+
+const getTeachingFeelingLauncherVersions = async (req, res) => {
+    const versions = await getVersionsFromGitHub('SpanishHgames', 'teaching-feeling---Launcher');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(versions, null, 2));
 };
 
 module.exports = {
-    getAllVersions,
+    getNaisTrainingDiaryVersions,
+    getTeachingFeelingLauncherVersions,
 };
