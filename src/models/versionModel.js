f104cd0c39d328eec1a34e3a1c5240cd5b7c9616
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-07-05 13:17:36 -0600

    update in the versioning api

--- src/models/versionModel.js
+++ src/models/versionModel.js
@@ -1,6 +1,28 @@
-const versions = [
-    { id: 1, version: '1.0.0', description: 'Initial release' },
-    { id: 2, version: '1.1.0', description: 'Minor update' },
-];
+const axios = require('axios');
 
-module.exports = versions;
+const GITHUB_API_BASE_URL = 'https://api.github.com/repos';
+
+const getVersionsFromGitHub = async (owner, repo) => {
+    try {
+        const tagsResponse = await axios.get(`${GITHUB_API_BASE_URL}/${owner}/${repo}/tags`);
+        const releasesResponse = await axios.get(`${GITHUB_API_BASE_URL}/${owner}/${repo}/releases`);
+        
+        const versions = tagsResponse.data.map(tag => {
+            const release = releasesResponse.data.find(release => release.tag_name === tag.name);
+            return {
+                id: tag.commit.sha,
+                version: tag.name,
+                description: release ? release.body : 'No description available'
+            };
+        });
+
+        return versions;
+    } catch (error) {
+        console.error('Error fetching data from GitHub:', error);
+        return [];
+    }
+};
+
+module.exports = {
+    getVersionsFromGitHub,
+};
